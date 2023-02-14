ad6b49754b26bb4a583999c9083f623f02434944
Sudhanshu Patel <patelsudhanshu290@gmail.com>
2023-02-15 03:12:04 +0530

    This is my LoslesVPN landing Page

---
+++ Assets/js/main.js
@@ -0,0 +1,43 @@
+// import './slider';
+
+let menu=document.querySelector("#menu-icon");
+let navbar=document.querySelector(".navbar");
+
+
+menu.onclick=()=>{
+    
+    menu.classList.toggle('ri-close-line');
+    navbar.classList.toggle('open');
+    
+}
+
+ 
+let swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints:{
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+        950: {
+            slidesPerView: 3,
+        },
+    },
+  });
